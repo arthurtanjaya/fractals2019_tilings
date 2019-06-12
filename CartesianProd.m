8b0a11efb08584c39ee5e7e614a1dadb2cc01374
Arthur Tanjaya <51280002+MathIsBae@users.noreply.github.com>
2019-06-12 11:16:57 -0400

    Update CartesianProd.m
    Added edge case n = 0

--- CartesianProd.m
+++ CartesianProd.m
@@ -1,9 +1,14 @@
 function product = CartesianProd(vector, n)
 %CARTESIANPROD Returns the n-fold Cartesian product of vector
+%   If n == 0 then this function returns {[]}
 % Taken from https://www.mathworks.com/matlabcentral/answers/282777-n-fold-cartesian-product
 
-product = repmat({vector}, 1, n);
-[product{1:n}] = ndgrid(product{:});
-product = reshape(cat(n, product{:}), [], n);
+if n == 0
+    product = {[]};  % Ugh. I can't think of a better answer...
+else
+    product = repmat({vector}, 1, n);
+    [product{1:n}] = ndgrid(product{:});
+    product = reshape(cat(n, product{:}), [], n);
+end
 
 end
