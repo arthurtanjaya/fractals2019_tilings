dfae333da37f6a7db3999d5eda3f5333494b7ae5
Arthur Tanjaya <51280002+MathIsBae@users.noreply.github.com>
2019-06-12 14:23:12 -0400

    Update GraphApprox.m
    Created vertex generation. Seems to work.

--- GraphApprox.m
+++ GraphApprox.m
@@ -1,6 +1,5 @@
 classdef GraphApprox
     %GRAPHAPPROX Graph Approximation of SG
-    %   ???
 
     properties (SetAccess = private)
         level     % Level of fineness (this is m in \Gamma_m)
@@ -23,15 +22,30 @@ classdef GraphApprox
                 % Construct V_{i-1} from V_{i-2}
                 new_vertices = Vertex.empty(0, 3^(i-1));
                 count = 1;  % To index new_vertices
-                for vertex = self.vertices
-                    address = vertex.get_address();
-                    % Assign addresses in a cyclic fashion
-                    new_address = [address(1:i-1) ...
-                                   mod(address(i)+1, 3) ...
-                                   address(i+1:end)];
-                    new_vertices(count) = Vertex(new_address);
-                    count = count + 1;
+
+                % Instead of iterating over the vertices in V_{i-2},
+                % we iterate over the i-2 cells instead
+                % Cell addressing is unique, so no problems here
+                cell_addresses = CartesianProd([0, 1, 2], i-2);
+                for cell_index = 1:3^(i-2)
+                    if i == 2
+                        cell = [];  % Annoying edge case
+                    else
+                        cell = cell_addresses(cell_index);
+                    end
+                    for head = 0:2
+                        address = [head cell];
+                        % Pad the end of address with the last relevant bit
+                        address(end+1:level+1) = address(end);
+                        % Assign addresses in a cyclic fashion
+                        new_address = [address(1:i-1) ...
+                                       mod(address(i)+1, 3) ...
+                                       address(i+1:end)];
+                        new_vertices(count) = Vertex(new_address);
+                        count = count + 1;
+                    end
                 end
+
                 self.vertices = [self.vertices new_vertices];  % Save updates
             end
 
