9cf8aa1df1954dbec4da2a5ff427a1a11f11092c
Arthur Tanjaya <51280002+MathIsBae@users.noreply.github.com>
2019-06-12 16:04:25 -0400

    Bugfix ApplyIFS_2D.m
    Fixed annoying bug due to how MATLAB handles indexing of nested arrays

--- ApplyIFS_2D.m
+++ ApplyIFS_2D.m
@@ -3,16 +3,12 @@ function coord = ApplyIFS_2D(coord, word)
 %   Note that word should not include the first bit of a vertex address
 
 % Define parameters
-N = 3;                % Number of contractions
 r = 1/2;              % Contraction ratio
 q = [[0.5, 3^0.5/2];
      [0, 0];
      [1, 0]];         % Fixed points of respective contractions
-F = cell(1, 3);       % Contractions
-for i = 1:N
-    F{i} = @(x) r*(x-q(i)) + q(i);
-end
+F = @(x, i) r*(x-q(i+1, :)) + q(i+1, :);  % Contractions
 
-for i = word
-    coord = F{i+1}(coord);  % This annoying 0/1-indexing issue...
+for w = word
+    coord = F(coord, w);  % This annoying 0/1-indexing issue...
 end
