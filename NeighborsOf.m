16ea70cf81906e818b9e89d96965240d4726d5d2
Arthur Tanjaya <51280002+MathIsBae@users.noreply.github.com>
2019-06-12 18:20:36 -0400

    Delete unnecessary files

--- NeighborsOf.m
+++
@@ -1,7 +0,0 @@
-function neighbors = NeighborsOf(vertex)
-%NEIGHBORSOF Returns the neighboring vertices of vertex
-%   Should we return the address? How to encode the address?
-
-neighbors = [0 1 2];
-
-end
--- VertexToEdge.m
+++
@@ -1,7 +0,0 @@
-function u_edge = VertexToEdge(u_vertex)
-%VERTEXTOEDGE Converts a function on vertices to its differences on edges
-%   Not sure how to do this yet
-
-u_edge = u_vertex;
-
-end
