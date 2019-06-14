d886d042544a4c4ae0f7cfab7501fa9ea858c37d
Sofya Bykova <sab444@cornell.edu>
2019-06-13 23:00:04 -0400

    different (numeric) approach to finding orthogonal bases

--- EF.m
+++ EF.m
@@ -7,7 +7,7 @@ npts = length(GraphApprox(level).vertices);
 syms x [1 npts];
 laplvar = GLaplacian(GraphApprox(level),x);
 laplparvar = laplvar(4:npts);
-eigenfunction = solve([laplparvar == -eigenvalue*x(4:npts), x(1:3) == [0,0,0]], x, 'ReturnCondition', true);
+eigenfunction = solve([laplparvar == -eigenvalue*x(4:npts), x(1:3) == [0,0,0], norm(x)~= 0], x, 'ReturnCondition', true);
 end
 
 function EFandEV = EigenfunctionsAll(level)
@@ -17,7 +17,7 @@ syms x [1 npts];
 syms ev;
 laplvar = GLaplacian(GraphApprox(level),x);
 laplparvar = laplvar(4:npts);
-EFandEV = solve([laplparvar == -ev*x(4:npts), x(1:3) == [0,0,0]], [x, ev], 'ReturnCondition', true);
+EFandEV = solve([laplparvar == -ev*x(4:npts), x(1:3) == [0,0,0], norm(x)^2~=0], [x, ev], 'ReturnCondition', true);
 end
 
 function eigenfunctionCheck = EigenfunctionCheck(level,eigenvalue,eigenfunction)
@@ -28,21 +28,20 @@ laplpar = lapl(4:npts);
 eigenfunctionCheck = isequal(laplpar,-eigenvalue*eigenfunction(4:npts));
 end
 
-function orthCheck = OrthogonalityCheck(functions)
+function orthArray = OrthogonalityCheck(functions)
 orthArray = [];
 [nfns,~] = size(functions);
 for i = 1 : nfns
     for j = 1 : (i-1)
          orthArray = [orthArray, dot(functions(i,:),functions(j,:))];
     end
-    orthArray = [orthArray, norm(functions(i,:))-1];
+    orthArray = [orthArray, (norm(functions(i,:)))^2-1];
 end
-orthCheck = orthArray == zeros([1, (nfns * (nfns + 1)/2)]);
 end
 
 function orthB = FindOrth(efBasis)
 par = length(efBasis.parameters);
-dim = length(efBasis.ev) - 1;
+dim = length(efBasis.ev);
 syms coefs [dim par];
 x = [];
 qBasis = [];
@@ -54,9 +53,30 @@ end
 for j = 1 : dim
     qBasis = [qBasis; subs(x(j,:), efBasis.parameters, coefs(j,:))];
 end
-qBasis
-EF.OrthogonalityCheck(qBasis)
-orthB = solve(EF.OrthogonalityCheck(qBasis));
+orthB = solve(EF.OrthogonalityCheck(qBasis) == zeros([1, (dim * (dim + 1)/2)]));
+end
+
+function orthB = FindOrthTwo(efBasis)
+par = length(efBasis.parameters);
+dim = length(efBasis.ev);
+
+    function orth = F(coefs)
+    x = [];
+    qBasis = [];
+    i = 1;
+    while isfield(efBasis,strcat('x',int2str(i)))
+        x = [x, getfield(efBasis,strcat('x',int2str(i)))];
+        i = i + 1;
+    end
+    for j = 1 : dim
+        qBasis = [qBasis; subs(x(j,:), efBasis.parameters, coefs(j,:))];
+    end
+    orth = double(EF.OrthogonalityCheck(qBasis));
+    end
+    
+
+fun = @F;
+orthB = fsolve(fun, ones([dim, par]));
 end
 end
 end
